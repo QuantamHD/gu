aba5e04a4a8d9f9f744c113bcdb514e0eaa8af2f
Kyle McCrohan <kmccrohan@zagmail.gonzaga.edu>
2017-05-01 18:06:34 -0700

    Added function to print out request data

--- utils/paperapp.go
+++ utils/paperapp.go
@@ -288,7 +288,8 @@ func submitDocument(credentials *PaperCutCredentials, printJob *PaperCutPrintJob
 
 	body := new(bytes.Buffer)
 	writer := multipart.NewWriter(body)
-	part, err := writer.CreateFormFile("filename", fi.Name())
+	part, err := writer.CreateFormFile("file[]", fi.Name())
+
 
 	if err != nil {
 		log.Fatal(err)
@@ -317,6 +318,8 @@ func submitDocument(credentials *PaperCutCredentials, printJob *PaperCutPrintJob
 
 	addUploadHeaders(req, credentials.sessionID)
 
+	fmt.Println(formatRequest(req))
+
 	resp, err := netClient.Do(req)
 
 	if err != nil {
@@ -329,6 +332,33 @@ func submitDocument(credentials *PaperCutCredentials, printJob *PaperCutPrintJob
 	println("Test")
 
 	defer resp.Body.Close()
+}
+
+// formatRequest generates ascii representation of a request
+func formatRequest(r *http.Request) string {
+ // Create return string
+ var request []string
+ // Add the request string
+ url := fmt.Sprintf("%v %v %v", r.Method, r.URL, r.Proto)
+ request = append(request, url)
+ // Add the host
+ request = append(request, fmt.Sprintf("Host: %v", r.Host))
+ // Loop through headers
+ for name, headers := range r.Header {
+   name = strings.ToLower(name)
+   for _, h := range headers {
+     request = append(request, fmt.Sprintf("%v: %v", name, h))
+   }
+ }
+
+ // If this is a POST, add post data
+ if r.Method == "POST" {
+    r.ParseForm()
+    request = append(request, "\n")
+    request = append(request, r.Form.Encode())
+ }
+  // Return the request as a string
+  return strings.Join(request, "\n")
 }
 
 func addGetHeaders(req *http.Request, jsessionid string) {
